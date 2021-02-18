256b8d0ec9e39168fdab300665b6982604dd80cb
akbarhps <akbarputrasrg@gmail.com>
2021-02-18 18:25:47 +0700

    🧹 Refactor all my shitty code using David Snyder's codes

--- index.js
+++ index.js
@@ -1,105 +1,127 @@
-const express = require('express');
-const { reset } = require('nodemon');
-const socket = require('socket.io');
+const express = require("express")
+const socket = require("socket.io")
 
-// Setup App
-const PORT = process.env.PORT || 3000;
-const app = express();
-const server = app.listen(PORT,
-  console.log("Server running on port " + PORT)
-);
+const app = express()
+app.use(express.static("public"))
 
-// Send static file to client
-app.use(express.static('public'));
+Port = process.env.PORT || 3000
 
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var player1turn = true;
-var gameover = false;
-var count = 0;
-var winner;
+const server = app.listen(Port, () => console.log("Server running on port: " + Port))
 
-let connectedPlayer = [];
+// misc functions for generating a random string and for capitalizing the first letter of a work
+const randomString = (n) => {
+  return [...Array(n)].map(i => (~~(Math.random() * 36)).toString(36)).join('')
+}
 
-// Socket setup & pass server
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+class game {
+  constructor() {
+    this.id = randomString();
+    this.players = []
+    this.sockets = []
+    this.names = {}
+    this.turn = "o"
+  }
+
+  isFull() {
+    return this.players.length >= 2
+  }
+
+  add(socket, name) {
+    if (!this.isFull()) {
+      this.players.push(socket.id)
+      this.sockets.push(socket)
+      this.names[socket.id] = name
+      socket.join(this.id)
+    }
+  }
+
+  emit(socket, title, data, same = true) {
+    socket.broadcast.to(this.id).emit(title, data)
+    if (same) {
+      socket.emit(title, data)
+    }
+  }
+}
+
+
+const gameFromId = (id) => {
+  return games.find(g => g.id == id)
+}
+
+
+const games = []
 const io = socket(server);
-io.on('connection', (socket) => {
-  console.log(`${socket.id} is connected`);
-  connectedPlayer.push(socket.id);
-  socket.emit('players', connectedPlayer);
-  socket.broadcast.emit('players', connectedPlayer);
-  console.log(connectedPlayer);
 
-  // Handle chat event
-  socket.on('player-turn', function (index) {
-    userTurn(index);
-    socket.emit('state', {
-      board: arr,
-      player: player1turn,
-      moveCount: count,
-      isGameOver: gameover,
-      gameWinner: winner
-    });
-    socket.broadcast.emit('state', {
-      board: arr,
-      player: player1turn,
-      moveCount: count,
-      isGameOver: gameover,
-      gameWinner: winner
-    });
+io.sockets.on("connection", (socket) => {
+
+  //game this socket is in
+  let Game;
+
+  // shorthand for this sockets id
+  const id = socket.id;
+
+  // name of user who will connect to this socket
+  let name;
+
+  socket.on("click", (data) => {
+    if (Game.isFull()) {
+      Game.emit(socket, "click", data)
+    }
+  })
+
+  socket.on("createGame", data => {
+    Game = new game()
+    Game.add(socket, data.name)
+    name = data.name
+    games.push(Game)
+    socket.emit("createdGame", Game.id)
+    console.log(Game.id);
+    Game.emit(socket, "message", capitalize(data.name) + " Created the game")
   });
 
-  socket.on('reset', (data) => {
-    resetGame();
-    socket.emit('state', {
-      board: arr,
-      player: player1turn
-    });
-    socket.broadcast.emit('state', {
-      board: arr,
-      player: player1turn
-    });
-  });
-});
+  socket.on("getPlayer", player => {
+    socket.broadcast.to(Game.id).emit("setPlayer", player == "x" ? "o" : "x")
+  })
 
-function userTurn(index) {
-  if (arr[index] > 0) {
-    arr[index] = (player1turn) ? -1 : 0;	// Else, put symbol
-    player1turn = !player1turn;
-    count++;
-    checkwon();
-  }
-}
-
-function resetGame() {
-  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  count = 0;
-  player1turn = true;
-  gameover = false;
-}
-
-function checkwon() {				// Check if in current board state player has won
-  if (count === 9) {			// If 9 turns are over, and no one has won, game is draw
-    gameover = true;
-  }
-
-  for (i = 0; i < 3; i++)			// Check columns
-    if (arr[i] == (arr[i + 3]) && arr[i] == (arr[i + 6])) {
-      gameover = true;
-      winner = [i, i + 6];	// Store first and last coordinate of winning boxes
+  socket.on("JoinGame", data => {
+    Game = gameFromId(data.id)
+    if (Game && !Game.isFull()) {
+      Game.add(socket, data.name)
+      Game.emit(socket, "getPlayer",)
+      Game.emit(socket, "message", capitalize(data.name) + " Joined the game")
+      name = data.name
+    } else {
+      socket.emit("err", { error: "not found", msg: "There is no open game available width id: " + data.id })
     }
+  })
 
-  for (i = 0; i < 9; i += 3)			// Check rows
-    if (arr[i] == (arr[i + 1]) && arr[i] == (arr[i + 2])) {
-      gameover = true;
-      winner = [i, i + 2];	// Store first and last coordinate of winning boxes
+  // send a message to clients in the game to reset
+  socket.on("reset", () => {
+    Game.emit(socket, "reset")
+  })
+
+  // send a message to the other client in the game with a chat message
+  socket.on("message", msg => {
+    Game.emit(socket, "message", msg, false)
+  })
+
+  socket.on("disconnect", () => {
+    /* remove the player from the game,
+       delete the game if it is empty 
+       and if not send messages to the other client to reset and add a message announcing the player leaveing */
+    if (Game) {
+      Game.players.pop(Game.players.indexOf(id));
+      if (Game.players.length === 0) {
+        games.pop(games.indexOf(Game))
+      } else {
+        Game.emit(Game.sockets[0], "leftGame")
+        Game.emit(Game.sockets[0], "message", capitalize(name) + " left the game")
+      }
     }
-
-  for (i = 0, b = 4; i < 3; i += 2, b -= 2)	// Check Diagonals
-    if (arr[i] === (arr[i + b]) && arr[i] === (arr[i + b + b])) {
-      gameover = true;
-      winner = [i, i + b + b];	// Store first and last coordinate of winning boxes
-    }
-
-  console.log(gameover);
-  console.log(winner);
-}
+  })
+})
---
+++ public/script.js
@@ -0,0 +1,279 @@
+let tiles;
+let gameTiles = []
+let gameReady = false
+let socket
+let turn = "x"
+let myTurn
+let myname
+let gameOver = false
+let gameId;
+
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+const index = (i, j) => {
+  return 3 * i + j
+}
+
+// html that will be injected in the cells for the 'x' and 'o' animation
+const Otext = `<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><circle cx="50" cy="50" r="45" /></svg>`
+
+const Xtext = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+  <line class="path line" fill="none" stroke="#FF0000" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="34.4" y1="37.9" x2="95.8" y2="92.3"/>
+  <line class="path line second" fill="none" stroke="#FF0000" stroke-width="6" stroke-linecap="round" stroke-miterlimit="10" x1="95.8" y1="38" x2="34.4" y2="92.2"/>
+</svg>`
+
+// function that executes on page load
+function setup() {
+  // prevent p5 from adding a canvas to the window
+  noCanvas();
+
+  // these divs should be hidden until the game starts
+  $(".game").hide();
+  $(".chat-area").hide();
+
+  // shorthand for all items with 'cell' class
+  tiles = $(".cell")
+
+  // connect to socket
+  socket = io.connect(window.location.origin)
+  if (!socket) {
+    socket = io.connect(window.location.origin + ":3000")
+  }
+
+  // add a message received from the server to the chat area
+  socket.on("message", msg => {
+    addMsg(msg)
+  })
+
+  // set a tile with an i and j, and an 'x' or 'o' based on data from the server
+  socket.on("click", data => {
+    let [i, j] = data.index
+    move(tiles[index(j, i)], data.player)
+    turn = data.player === "x" ? "o" : "x";
+  })
+
+  // set the local gameId from the id received from the server
+  socket.on("createdGame", id => gameId = id)
+
+  // reset the game if the other person leaves the game
+  socket.on("leftGame", reset)
+
+  // alert the user to an error if the server sends one
+  socket.on("err", data => {
+    if (data.error === "not found") {
+      // a game wasn't found so rehide these things
+      $(".input").show()
+      $(".game").hide();
+    }
+    setTimeout(() => {
+      alert(data.msg)
+    }, 150);
+  })
+
+  // send the server which player you are when asked
+  socket.on("getPlayer", () => {
+    socket.emit("getPlayer", myTurn)
+  })
+
+  // set my player based on data from the server
+  socket.on("setPlayer", player => {
+    myTurn = player
+    $(".myturn").html(myname + ", You Are " + player.toUpperCase())
+  })
+
+  // try to reset the game if the server tells you to
+  socket.on("reset", () => {
+    reset(false)
+  })
+}
+
+// get the i and j position of a tile
+function getIndex(tile) {
+  let i = Number($(tile).css("grid-column")[0]) - 1
+  let j = Number($(tile).css("grid-row")[0]) - 1
+  return [i, j]
+}
+
+function reset(clicked = true) {
+  // only reset when game is over
+  if (gameOver) {
+    for (let t of tiles) {
+      t = $(t)
+      // clear the values and innerhtml for all the cells
+      t.val("")
+      t.html("")
+    }
+    // clear the winner and set it to x's turn cause x goes first
+    $(".winner").html("Winner: ")
+    $(".turn").html("X's Turn")
+    turn = "x"
+    gameOver = false
+    // if the button on the webpage was clicked, send a message for others to reset, if I received a message to reset then don't
+    if (clicked) {
+      socket.emit("reset")
+    }
+  }
+}
+
+// get all the values from the cells to be used the in the winner function
+function getvals() {
+  let r = []
+  for (let t of tiles) {
+    r.push($(t).val())
+  }
+  return r
+}
+
+function winner() {
+  let board = values = getvals()
+
+  let first = values[0]
+  let diagonal = first != ""
+  for (let i = 0; i < 3; i++) {
+    if (values[index(i, i)] != first) {
+      diagonal = false
+      break
+    }
+  }
+
+  if (diagonal) return first
+
+  first = board[index(0, 2)]
+  let back_diag = first != ""
+  for (let i = 1; i < 4; i++) {
+    if (board[index(i - 1, 3 - i)] != first) {
+      back_diag = false
+      break
+    }
+  }
+  if (back_diag) return first
+
+  for (let i = 0; i < 3; i++) {
+    first = board[index(i, 0)]
+    let sideways = first != ""
+    for (let j = 0; j < 3; j++) {
+      if (board[index(i, j)] != first) {
+        sideways = false
+        break
+      }
+    }
+    if (sideways) return first
+  }
+
+  for (let i = 0; i < 3; i++) {
+    first = board[index(0, i)]
+    let sideways = first != ""
+    for (let j = 0; j < 3; j++) {
+      if (board[index(j, i)] != first) {
+        sideways = false
+        break
+      }
+    }
+    if (sideways) return first
+  }
+  return values.filter((v) => v === "").length === 0 ? "tie" : undefined
+}
+
+
+function move(tile, player) {
+  if ($(tile).html() === "") {
+    $(tile).html(player === "o" ? Otext : Xtext)
+    $(tile).val(player)
+    player = player === "x" ? "o" : "x"
+    $(".turn").html(player.toUpperCase() + "'s Turn")
+  }
+  if (winner()) {
+    $(".winner").html("Winner: " + winner().toUpperCase())
+    gameOver = true
+  }
+}
+
+// when a cell div is clicked figure out if it already has a move there and if not send a message telling all sockets to set a move there
+$('.cell').click(function () {
+  if ($(this).html() === "" && turn === myTurn && !gameOver) {
+    let index = getIndex(this)
+    let player = turn
+    console.log("clicked")
+    socket.emit("click", { index, player })
+  }
+});
+
+// prevent XSS
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
+
+
+$(".create-button").click(function () {
+  let name = $("#create-name").val()
+  name = escapeHtml(name)
+  if (name) {
+    socket.emit("createGame", { name })
+    $(".input").hide()
+    $(".game").show();
+    $(".chat-area").show();
+    select("#game").removeClass("expand")
+    myTurn = "x"
+    myname = name
+    $(".myturn").html(name + ", You Are X")
+    setTimeout(() => {
+      $(".id").html("Game ID: " + gameId)
+    }, 300);
+  }
+})
+
+$(".join-button").click(function () {
+  let name = $("#join-name").val()
+  let id = $("#game-id").val()
+  name = escapeHtml(name)
+  id = escapeHtml(id)
+  if (name && id) {
+    let data = { name, id }
+    socket.emit("JoinGame", data)
+    $(".input").hide()
+    $(".game").show();
+    $(".chat-area").show();
+    select("#game").removeClass("expand")
+    myTurn = "o"
+    myname = name
+    $(".myturn").html(name + ", You Are O")
+    setTimeout(() => {
+      $(".id").html("Game ID: " + id)
+    }, 300);
+  }
+})
+
+// add a message the chat area div
+function addMsg(msg) {
+  createDiv(escapeHtml(msg)).addClass("message").parent("messages")
+}
+
+// toggle hiding and showing the extra game info that is currently only the game id
+$(".info-button").click(function () {
+  $(".inner-info").toggleClass("show")
+})
+
+// send a message if there is one to send when the 'send' button is pressed
+$(".send").click(() => {
+  let msg = $(".msg-input").val()
+  if (msg) {
+    addMsg(msg)
+    $(".msg-input").val("")
+    socket.emit("message", capitalize(myname) + ": " + msg)
+  }
+})
+
+// link the enter key to sending a message
+$(document).keyup(function (event) {
+  if (event.keyCode === 13) {
+    $(".send").click();
+  }
+});
